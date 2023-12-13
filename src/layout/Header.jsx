f41d16c89a2400d88c7b762ab620a1fa760c29d9
Tatsiana Kmit <drbeatallica@yandex.ru>
2023-12-13 12:28:43 +0300

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return <nav className="#009688 teal">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">React Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">Sass</a></li>
+       
+      </ul>
+    </div>
+  </nav>
+
+}
+
+export {Header}
